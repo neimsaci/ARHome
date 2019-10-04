a46b2aeee95002d120ddbb09de1cf0d46fc12786
Rachel Chen <rachel@twobulls.com>
2019-10-04 12:45:05 -0400

    Initial Commit

---
+++ ARHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARHome
+//
+//  Created by Rachel Chen on 10/4/19.
+//  Copyright © 2019 Rachel Chen. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
