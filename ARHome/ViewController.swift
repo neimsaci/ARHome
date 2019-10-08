e57887025d78ed7de6324edf09f8195dd0bc4d3e
Rachel Chen <rachel@twobulls.com>
2019-10-08 11:47:36 -0400

    Add clear button

--- ARHome/ViewController.swift
+++ ARHome/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        clearAllFurniture()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,24 +31,24 @@ class ViewController: UIViewController {
     }
     
     @objc func handleScreenTap(sender: UITapGestureRecognizer) {
-      let tapLocation = sender.location(in: arView)
+        let tapLocation = sender.location(in: arView)
 
-      // Find all the detected planes that would intersect with
-      // a line extending from where the user tapped the screen.
-      let planeIntersections = arView.hitTest(
-        tapLocation,
-        types: [.estimatedHorizontalPlane])
+        // Find all the detected planes that would intersect with
+        // a line extending from where the user tapped the screen.
+        let planeIntersections = arView.hitTest(
+            tapLocation,
+            types: [.estimatedHorizontalPlane])
 
-      // If the closest of those planes is horizontal,
-      // put the current furniture item on it.
-      if !planeIntersections.isEmpty {
-        addItem(hitTestResult: planeIntersections.first!)
-      }
+        // If the closest of those planes is horizontal,
+        // put the current furniture item on it.
+        if !planeIntersections.isEmpty {
+            addItem(hitTestResult: planeIntersections.first!)
+        }
     }
     
     func addItem(hitTestResult: ARHitTestResult) {
-      // Get the real-world position corresponding to
-      // where the user tapped on the screen.
+        // Get the real-world position corresponding to
+        // where the user tapped on the screen.
         let transform = hitTestResult.worldTransform
         let positionColumn = transform.columns.3
         let product = try! Entity.loadModel(named: "chair_swan")
@@ -53,5 +57,9 @@ class ViewController: UIViewController {
         let placementAnchor = AnchorEntity(plane: .horizontal, minimumBounds: [0.1, 0.1])
         arView.scene.addAnchor(placementAnchor)
         placementAnchor.addChild(product)
+    }
+    
+    func clearAllFurniture() {
+        arView.scene.anchors.removeAll()
     }
 }
